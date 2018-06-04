bb33fb75d46c042e605c46d1f85086b0b102e3e0
Pruthula.S <pruthula.s@siemens.com>
2018-06-04 16:54:14 +0530

    Correcting a test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import static org.junit.Assert.fail;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class DemoApplicationTests {
@@ -11,7 +10,7 @@ public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() throws Exception {
-		fail();
+		
 	}
 
 }
